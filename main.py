d4de78908d74b4d0db26658ca6623be057393401
blCkc0der <danny.chosen100@gmail.com>
2025-04-14 21:58:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ import httpx
 
 app = Flask(__name__)
 
-OPENAI_API_KEY = "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
-"
+OPENAI_API_KEY = "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"
 OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
 OPENAI_TTS_URL = "https://api.openai.com/v1/audio/speech"
 headers = {
