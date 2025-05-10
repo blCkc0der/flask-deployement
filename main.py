c593fc7e347ae45832228d8d0b5260e042592dbe
macbook <macbook@renadd-D.local>
2025-05-11 02:55:40 +0400

    Fix NameError by importing render_template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file, render_template_string
+from flask import Flask, request, jsonify, send_file, render_template
 from gtts import gTTS
 import cv2
 import time
